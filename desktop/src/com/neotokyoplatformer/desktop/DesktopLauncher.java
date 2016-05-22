1a8eecf405caaa8a5722cecafcc881bec586a220
sifusan <fuglestad.simen@gmail.com>
2016-05-22 18:56:30 +0200

    basic setup with intellij project files

---
+++ desktop/src/com/neotokyoplatformer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.neotokyoplatformer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.neotokyoplatformer.neotokyo-platformer-main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new neotokyo-platformer-main(), config);
+	}
+}
